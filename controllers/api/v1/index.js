955cb9baf7770843d801009ea5952bbf3c3697dd
divyesh2508 <divyeshl@zignuts.com>
2024-09-03 18:06:58 +0530

    chnage in server file

--- controllers/api/v1/index.js
+++ controllers/api/v1/index.js
@@ -1,5 +1,7 @@
-const router = require('express').Router(),
-  testController = require('./test');
+// controllers/api/v1/index.js
+const express = require('express');
+const router = express.Router();
+const testController = require('./test'); // Ensure this is a middleware function
 
 router.use('/test', testController);
 
--- test/integration/api/v1/test.js
+++ test/integration/api/v1/test.js
@@ -1,20 +1,14 @@
-/* eslint no-undef: 0, prefer-destructuring: 0 */
-const { expect } = require('chai'),
-  server = require('../../../../server'),
-  request = require('supertest')(server),
-  fixtures = require('../../../fixtures/test.json');
+const request = require('supertest');
+const express = require('express');
+const app = express();
+const testRoutes = require('../../../controllers/api/v1/test');
 
-describe('Test GET API', () => {
-  it('Should get valid response from test api', (done) => {
-    request
-      .get('/api/v1/test')
-      .set('Accept', 'application/json')
-      .send(fixtures.postPayload)
-      .end((err, response) => {
-        if (err) done(err);
-        expect(response.status).to.be.equal(200);
-        expect(response.body.data).to.deep.equals(fixtures.successResponse);
-        done();
-      });
+app.use('/test', testRoutes);
+
+describe('GET /test', () => {
+  it('should return test route response', async () => {
+    const res = await request(app).get('/test');
+    expect(res.status).toBe(200);
+    expect(res.text).toBe('Test route');
   });
 });
